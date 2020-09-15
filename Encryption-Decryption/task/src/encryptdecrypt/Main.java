9dd3db67fd9d8c7a42eb4052d1b1bdf3cd845587
ginowine <ginowine1@gmail.com>
2020-09-15 22:56:07 +0100

    caesar cipher encryption and decryption

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -4,73 +4,35 @@ import java.util.Scanner;
 
 public class Main {
 
-    static String cipherText = "";
-    static char alphabet;
-    static String targetOperation = "";
-    static String input = "";
-    static int key;
-    static String decryptMessage = "";
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        targetOperation = scanner.nextLine();
-        input = scanner.nextLine();
-        key = scanner.nextInt();
-        if (targetOperation.equals("enc")){
-            encryption();
-        }else if (targetOperation.equals("dec")){
-            decryption();
-        }
-
-        System.out.println(cipherText);
-    }
-
-    private static void decryption() {
-        for (int i = 0; i < input.length(); i++){
-            alphabet = input.charAt(i);
-            if (alphabet >= 'a' && alphabet <= 'z'){
-                alphabet = (char) (alphabet - key);
-                if (alphabet < 'a'){
-                    alphabet = (char) (alphabet - 'a'+'z'+1);
-                }
-                //decryptMessage = decryptMessage + alphabet;
-                cipherText = cipherText + alphabet;
-            }else if (alphabet >= 'A' && alphabet <= 'Z'){
-                alphabet = (char) (alphabet - key);
-
-                if (alphabet < 'A'){
-                    alphabet = (char) (alphabet-'A'+'Z'+1);
-                }
-                cipherText = cipherText + alphabet;
-            }else {
-                cipherText = cipherText + alphabet;
-            }
-            //System.out.println(cipherText);
+        String operation = scanner.nextLine();
+        String message = scanner.nextLine();
+        int key = scanner.nextInt();
+        if (operation.equals("enc")) {
+            encrypt(message, key);
+        } else if (operation.equals("dec")) {
+            decrypt(message, key);
         }
     }
 
-    public static void encryption(){
-        for (int i = 0; i < input.length(); i++){
-            alphabet = input.charAt(i);
-
-            if (alphabet >= 'a' && alphabet <= 'z'){
-                alphabet = (char) (alphabet + key);
-
-                if (alphabet > 'z'){
-                    alphabet = (char) (alphabet+'a'-'z'-1);
-                }
-                cipherText = cipherText + alphabet;
-            }else if (alphabet >= 'A' && alphabet <='Z'){
-                alphabet = (char) (alphabet + key);
-
-                if (alphabet > 'Z'){
-                    alphabet = (char) (alphabet+'A'-'Z'-1);
-                }
-                cipherText = cipherText + alphabet;
-            }else {
-                cipherText = cipherText + alphabet;
-            }
+    public static String encrypt(String msg, int key) {
+        StringBuilder encryptedMessage = new StringBuilder();
+        for (int i = 0; i < msg.length(); i++) {
+            int index = msg.charAt(i);
+            encryptedMessage.append((char) (index + key));
         }
-        //System.out.println(cipherText);
+        System.out.println(encryptedMessage);
+        return encryptedMessage.toString();
+    }
+
+    public static String decrypt(String msg, int key) {
+        StringBuilder encryptedMessage = new StringBuilder();
+        for (int i = 0; i < msg.length(); i++) {
+            int index = msg.charAt(i);
+            encryptedMessage.append((char) (index - key));
+        }
+        System.out.println(encryptedMessage);
+        return encryptedMessage.toString();
     }
 }
