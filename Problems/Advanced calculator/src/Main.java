cce5a1726fe9927fef684ffa11cbd1c7e9cd0c19
ginowine <ginowine1@gmail.com>
2020-09-17 20:31:29 +0100

    advance calculator fixed

--- Problems/Advanced calculator/src/Main.java
+++ Problems/Advanced calculator/src/Main.java
@@ -5,43 +5,35 @@ class Problem {
         String operator = args[0];
         // write your code here
 
-        switch (operator){
+        switch (operator) {
             case "MAX":
-                getMax(args);
+                int maxValue = Integer.parseInt(args[1]);
+                for (int i = 1; i < args.length; i++) {
+                    if (Integer.parseInt(args[i]) > maxValue) {
+                        maxValue = Integer.parseInt(args[i]);
+                    }
+                }
+                System.out.println(maxValue);
                 break;
             case "MIN":
-                getMin(args);
+                int minValue = Integer.parseInt(args[1]);
+                for (int i = 1; i < args.length; i++) {
+                    if (Integer.parseInt(args[i]) < minValue) {
+                        minValue = Integer.parseInt(args[i]);
+                    }
+                }
+                System.out.println(minValue);
                 break;
             case "SUM":
-                getSum(args);
+                int sum = 0;
+                for (int i = 1; i < args.length; i++) {
+                    sum += Integer.parseInt(args[i]);
+                }
+                System.out.println(sum);
+                break;
+            default:
+                System.out.println("Unknown operator");
                 break;
         }
-    }
-
-    private static void getSum(String[] args) {
-        int sum = 0;
-        for (int i = 1; i < args.length; i++){
-            sum += Integer.parseInt(args[i]);
-        }
-        System.out.println(sum);
-    }
-
-    private static void getMin(String[] args) {
-        int minValue = Integer.parseInt(args[0]);
-        for (int i = 1; i < args.length; i++){
-            if (Integer.parseInt(args[0]) < minValue){
-                minValue = Integer.parseInt(args[i]);
-            }
-        }
-        System.out.println(minValue);
-    }
-    private static void getMax(String[] args) {
-        int maxValue = Integer.parseInt(args[0]);
-        for (int i = 1; i < args.length; i++){
-            if (Integer.parseInt(args[i]) > maxValue){
-                maxValue = Integer.parseInt(args[i]);
-            }
-        }
-        System.out.println(maxValue);
     }
 }
