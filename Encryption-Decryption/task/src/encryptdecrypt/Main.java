51e4b436ed83118a9676e912cfc1d92ce00669f0
ginowine <ginowine1@gmail.com>
2020-09-24 16:19:24 +0100

    writing to file in Java

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,10 +1,18 @@
 package encryptdecrypt;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
     public static void main(String[] args) {
         int key = 0;
         String data = "";
         String mode = "";
+        String readData = "";
+        String writeData = "";
+
+        //String fileUrl = "C:\\Users\\Gino\\Documents\\JavaCodes\\gino.txt";
 
         for (int i = 0; i < args.length; i += 2) {
             String command = args[i];
@@ -18,6 +26,12 @@ public class Main {
                 case "-key":
                     key = Integer.parseInt(args[i+1]);
                     break;
+                case "-in":
+                    readData = args[i+1];
+                    break;
+                case "-out":
+                    writeData = args[i+1];
+                    break;
             }
         }
 
@@ -26,12 +40,30 @@ public class Main {
                 decrypt(data, key);
                 break;
             default:
-                encrypt(data, key);
+                encrypt(data, key, readData);
         }
     }
-    public static void encrypt(String message, int key) {
-        for (int i = 0; i < message.length(); i++) {
-            int ch = message.charAt(i);
+
+    public static String readFileAsString(String fileName) throws IOException{
+        return new String(Files.readAllBytes(Paths.get(fileName)));
+    }
+    public static void encrypt(String message, int key, String readFromFile) {
+        String sourceOfData = "";
+        try {
+            sourceOfData = readFileAsString(readFromFile);
+        }catch (IOException e){
+            System.out.println("Cannot read file: " + e.getMessage());
+        }
+
+        if (message.isEmpty()){
+            sourceOfData = sourceOfData;
+        }else {
+            sourceOfData = message;
+        }
+
+
+        for (int i = 0; i < sourceOfData.length(); i++) {
+            int ch = sourceOfData.charAt(i);
             ch += key;
             System.out.print((char) ch);
         }
