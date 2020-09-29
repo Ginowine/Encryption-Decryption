e5c89870e789b353b411ce6d1aeab785dbe0dbb3
ginowine <ginowine1@gmail.com>
2020-09-29 09:41:28 +0100

    constructor overloading

---
+++ Problems/Employees/src/Main.java
@@ -0,0 +1,59 @@
+class Employee {
+
+    // write fields
+    protected String name;
+    protected String email;
+    protected int experience;
+
+    // write constructor
+    public Employee(String name, String email, int experience){
+        this.name = name;
+        this.email = email;
+        this.experience = experience;
+    }
+
+    // write getters
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public int getExperience(){
+        return experience;
+    }
+}
+
+class Developer extends Employee {
+
+    // write fields
+    protected String mainLanguage;
+    protected String skills;
+
+    // write constructor
+    public Developer(String name, String email, int experience, String mainLanguage, String skills){
+        super(name, email, experience);
+        this.mainLanguage = mainLanguage;
+        this.skills = skills;
+    }
+
+    // write getters
+    public String getMainLanguage(){
+        return mainLanguage;
+    }
+
+    public String getSkills(){
+        return skills;
+    }
+}
+
+class DataAnalyst extends Employee {
+
+    // write fields
+
+    // write constructor
+
+    // write getters
+}
