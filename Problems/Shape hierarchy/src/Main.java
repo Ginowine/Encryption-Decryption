4c122a21ce73bb727aae83e69026a17ad20815b1
ginowine <ginowine1@gmail.com>
2020-10-05 00:57:17 +0100

    calculating perimeter and area of shapes

--- Problems/Shape hierarchy/src/Main.java
+++ Problems/Shape hierarchy/src/Main.java
@@ -1,17 +1,3 @@
-public class Main { // add this class to test output in IDE
-    public static void main(String[] args) {
-        Circle circle = new Circle(10);
-        Triangle triangle = new Triangle(3, 4, 5);
-        Rectangle rectangle = new Rectangle(5, 10);
-
-        System.out.println("Circle perimeter: " + circle.getPerimeter() + " area: " + circle.getArea());
-        System.out.println("Triangle perimeter: " + triangle.getPerimeter() + " area: " + triangle.getArea());
-        System.out.println("Rectangle perimeter: " + rectangle.getPerimeter() + " area: " + rectangle.getArea());
-
-    }
-}
-
-
 abstract class Shape {
     abstract double getPerimeter();
     abstract double getArea();
@@ -27,7 +13,7 @@ class Triangle extends Shape{
 
     public Triangle(double a, double b, double c ){
         this.a = a;
-        this.b = c;
+        this.b = b;
         this.c = c;
     }
 
