74fdc351fff989cd72046cc94005c5b3ef673423
ginowine <ginowine1@gmail.com>
2020-10-04 22:55:08 +0100

    overriding methods of superclass

--- Problems/Users and WebSites/src/Main.java
+++ Problems/Users and WebSites/src/Main.java
@@ -1,25 +1,31 @@
-class User {
-
+abstract class BaseEntity{
     protected long id;
-
     protected long version;
 
+    protected BaseEntity(long id, long version){
+        this.id = id;
+        this.version = version;
+    }
+
+    public abstract long getId();
+
+    public abstract void setId(long id);
+
+    public abstract long getVersion();
+
+    public abstract void setVersion(long version);
+
+}
+
+class User extends BaseEntity {
     protected String name;
 
-    public long getId() {
-        return id;
+    public User(){
+        super(0, 0);
     }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public long getVersion() {
-        return version;
-    }
-
-    public void setVersion(long version) {
-        this.version = version;
+    public User(long id, long version, String name) {
+        super(id, version);
+       this.name = name;
     }
 
     public String getName() {
@@ -29,32 +35,40 @@ class User {
     public void setName(String name) {
         this.name = name;
     }
-}
 
-class Visit {
-
-    protected long id;
-
-    protected long version;
-
-    protected User user;
-
-    protected WebSite site;
-
+    @Override
     public long getId() {
         return id;
     }
 
+    @Override
     public void setId(long id) {
         this.id = id;
     }
 
+    @Override
     public long getVersion() {
         return version;
     }
 
+    @Override
     public void setVersion(long version) {
         this.version = version;
+    }
+}
+
+class Visit extends BaseEntity{
+    protected User user;
+    protected WebSite site;
+
+    public Visit(){
+        super(0, 0);
+    }
+
+    public Visit(long id, long version, User user, WebSite site) {
+        super(id, version);
+        this.user = user;
+        this.site = site;
     }
 
     public User getUser() {
@@ -72,30 +86,39 @@ class Visit {
     public void setSite(WebSite site) {
         this.site = site;
     }
+
+    @Override
+    public long getId() {
+        return 0;
+    }
+
+    @Override
+    public void setId(long id) {
+
+    }
+
+    @Override
+    public long getVersion() {
+        return 0;
+    }
+
+    @Override
+    public void setVersion(long version) {
+
+    }
 }
 
-class WebSite {
-
-    protected long id;
-
-    protected long version;
+class WebSite extends BaseEntity{
 
     protected String url;
 
-    public long getId() {
-        return id;
+    public WebSite(){
+        super(0, 0);
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public long getVersion() {
-        return version;
-    }
-
-    public void setVersion(long version) {
-        this.version = version;
+    public WebSite(long id, long version, String url) {
+        super(id, version);
+        this.url = url;
     }
 
     public String getUrl() {
@@ -104,5 +127,25 @@ class WebSite {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    @Override
+    public long getId() {
+        return 0;
+    }
+
+    @Override
+    public void setId(long id) {
+
+    }
+
+    @Override
+    public long getVersion() {
+        return 0;
+    }
+
+    @Override
+    public void setVersion(long version) {
+
     }
 }
