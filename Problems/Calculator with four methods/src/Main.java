37da14264046700e573c6eac361e93e59ffba0b2
ginowine <ginowine1@gmail.com>
2020-11-09 08:31:19 +0100

    initial commit

--- Problems/Calculator with four methods/src/Main.java
+++ Problems/Calculator with four methods/src/Main.java
@@ -10,33 +10,46 @@ class SimpleCalculator {
         String operator = scanner.next();
         long num2 = scanner.nextLong();
 
+        switch (operator) {
+            case "+":
+                sumTwoNumbers(num1, num2);
+                break;
+            case "-":
+                subtractTwoNumbers(num1, num2);
+                break;
+            case "/":
+                divideTwoNumbers(num1, num2);
+                break;
+            case "*":
+                multiplyTwoNumbers(num1, num2);
+                break;
+            default:
+                break;
+        }
+
     }
 
     // Implement your methods here
-    public static void subtractTwoNumbers(long num1, long num2){
-       long result = num1 - num2;
-       System.out.println(result);
+    public static void subtractTwoNumbers(long num1, long num2) {
+        System.out.println(num1 - num2);
     }
 
 
-    public static void sumTwoNumbers(long num1, long num2){
-        long result = num1 + num2;
-        System.out.println(result);
+    public static void sumTwoNumbers(long num1, long num2) {
+        System.out.println(num1 + num2);
     }
 
 
-    public static void divideTwoNumbers(long num1, long num2){
-        if (num2 == 0){
+    public static void divideTwoNumbers(long num1, long num2) {
+        if (num2 == 0) {
             System.out.println("Division by 0!");
-        }else {
-            long result = num1 / num2;
-            System.out.println(result);
+        } else {
+            System.out.println(num1 / num2);
         }
     }
 
 
-    public static void multiplyTwoNumbers(long num1, long num2){
-        long result = num1 * num2;
-        System.out.println(result);
+    public static void multiplyTwoNumbers(long num1, long num2) {
+        System.out.println(num1 * num2);
     }
 }
