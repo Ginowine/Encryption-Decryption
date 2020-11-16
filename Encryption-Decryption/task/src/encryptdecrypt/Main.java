8679c4a648674dea076ddd39fc8a9b10b8d72822
ginowine <ginowine1@gmail.com>
2020-11-16 19:46:10 +0100

    template methods

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,108 +1,164 @@
 package encryptdecrypt;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.Scanner;
 
-public class Main {
+abstract class Algorithm {
 
-    static String writeData = "";
-    static String data = "";
-    static String readData = "";
-    public static void main(String[] args) {
-        int key = 0;
-        String data = "";
-        String mode = "";
+    abstract String encrypt(String message, int key);
 
+    abstract String decipher(String message, int key);
+}
+
+class UnicodeAlgorithm extends Algorithm {
+
+    public String encrypt(String message, int key) {
+        StringBuilder cipherText = new StringBuilder();
+
+        for (char c : message.toCharArray()) {
+            cipherText.append((char) (c + key));
+        }
+
+        return cipherText.toString();
+    }
+
+    public String decipher(String message, int key) {
+        return encrypt(message, -key);
+    }
+}
+
+class ShiftAlgorithm extends Algorithm {
+
+    public String encrypt(String message, int key) {
+        StringBuilder plainText = new StringBuilder();
+
+        for (char c : message.toCharArray()) {
+            if (Character.isAlphabetic(c)) {
+                int shift = Character.isUpperCase(c) ? 65 : 97;
+                plainText.append((char) (modulo(c - shift + key) + shift));
+            } else {
+                plainText.append(c);
+            }
+        }
+
+        return plainText.toString();
+    }
+
+    public String decipher(String message, int key) {
+        return encrypt(message, -key);
+    }
+
+    private static int modulo(int x) {
+        return (x % 26 + 26) % 26;
+    }
+}
+
+class Enigma {
+    int key = 0;
+    String mode = "enc";
+    String data = "";
+    String inPath = "";
+    String outPath = "";
+    Algorithm algorithm = new ShiftAlgorithm();
+
+    public void execute() {
+        readData();
+
+        switch (this.mode) {
+            case "enc":
+                String cipherText = algorithm.encrypt(data, key);
+                output(cipherText, outPath);
+                break;
+            case "dec":
+                String plainText = algorithm.decipher(data, key);
+                output(plainText, outPath);
+                break;
+            default:
+                System.err.println("unknown mode");
+                System.exit(1);
+        }
+    }
+
+    /**
+     * Output encryption/decryption results to STDERR of "-out"
+     */
+    private static void output(String message, String outPath) {
+        if (outPath.equals("")) {
+            System.out.println(message);
+        } else {
+            File file = new File(outPath);
+
+            try (FileWriter writer = new FileWriter(file)) {
+                writer.write(message);
+            } catch (IOException e) {
+                System.err.println("can't write to " + outPath);
+                System.exit(1);
+            }
+        }
+    }
+
+    /**
+     * Read data from the file if "-in" was given and "-data" was not
+     */
+    private void readData() {
+        if (data.equals("")) {  // prefer "-data" over "-in"
+            String fileName = inPath;
+            File file = new File(fileName);
+            try (Scanner scanner = new Scanner(file)) {
+                data = scanner.nextLine();
+            } catch (FileNotFoundException e) {
+                System.err.println(fileName + " not found");
+                System.exit(1);
+            }
+        }
+    }
+
+    public void parseArgs(String[] args) {
         for (int i = 0; i < args.length; i += 2) {
-            String command = args[i];
-            switch (command) {
-                case "-mode":
-                    mode = args[i+1];
+            switch (args[i]) {
+                case "-alg":
+                    switch (args[i + 1]) {
+                        case "shift":
+                            algorithm = new ShiftAlgorithm();
+                            break;
+                        case "unicode":
+                            algorithm = new UnicodeAlgorithm();
+                            break;
+                        default:
+                            System.err.println("unknown algorithm " + args[i + 1]);
+                            System.exit(1);
+                    }
                     break;
-                case "-data":
-                    data = args[i+1];
+                case "-mode":
+                    mode = args[i + 1];
                     break;
                 case "-key":
-                    key = Integer.parseInt(args[i+1]);
+                    key = Integer.parseInt(args[i + 1]);
                     break;
                 case "-in":
-                    readData = args[i+1];
+                    inPath = args[i + 1];
+                    break;
+                case "-data":
+                    data = args[i + 1];
                     break;
                 case "-out":
-                    writeData = args[i+1];
+                    outPath = args[i + 1];
                     break;
-            }
-        }
-
-        if ("dec".equals(mode)) {
-            decrypt(data, key, readData);
-        } else {
-            encrypt(data, key, readData);
-        }
-    }
-
-    public static void encrypt(String message, int key, String readFromFile) {
-        String sourceOfData = "";
-        if (message.isEmpty()){
-            try {
-                data = readFileAsString(readFromFile);
-                sourceOfData = data;
-                File file = new File(writeData);
-                PrintWriter printWriter = new PrintWriter(file);
-                for (int i = 0; i < sourceOfData.length(); i++){
-                    int ch = sourceOfData.charAt(i);
-                    ch += key;
-                    printWriter.write((char) ch);
-                    printWriter.flush();
-                    //printWriter.close();
-                }
-            }catch (IOException e){
-                System.out.println("Error writing or reading data from file: " + e.getMessage());
-            }
-        }else {
-            sourceOfData = message;
-            for (int i = 0; i < sourceOfData.length(); i++){
-                int ch = sourceOfData.charAt(i);
-                ch += key;
-                System.out.print((char) ch);
+                default:
+                    System.err.println("unknown argument");
+                    System.exit(1);
             }
         }
     }
+}
 
-    public static void decrypt(String message, int key, String readDataFromFile) {
-        String sourceData = "";
-        if (message.isEmpty()){
-            try {
-                data = readFileAsString(readDataFromFile);
-                sourceData = data;
-
-                File file = new File(writeData);
-                PrintWriter printWriter = new PrintWriter(file);
-                for (int i = 0; i < sourceData.length(); i++){
-                    int ch = sourceData.charAt(i);
-                    ch -= key;
-                    printWriter.write((char) ch);
-                    printWriter.flush();
-                    //printWriter.close();
-                }
-            }catch (IOException e){
-                System.out.println("Error writing or reading data from file: " + e.getMessage());
-            }
-        }else {
-            sourceData = message;
-
-            for (int i = 0; i < sourceData.length(); i++) {
-                int ch = message.charAt(i);
-                ch -= key;
-                System.out.print((char) ch);
-            }
-        }
-    }
-
-    public static String readFileAsString(String fileName) throws IOException{
-        return new String(Files.readAllBytes(Paths.get(fileName)));
+public class Main {
+    public static void main(String[] args) {
+        Enigma enigma = new Enigma();
+        enigma.parseArgs(args);
+        enigma.execute();
     }
 }
