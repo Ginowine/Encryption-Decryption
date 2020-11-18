365d010beb2a66f7fd2bf2f16387860a01d0efdf
ginowine <ginowine1@gmail.com>
2020-11-18 14:45:56 +0100

    removing digits from a string array

---
+++ Problems/Concat all strings without digits/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class ConcatenateStringsProblem {
+
+    public static String concatenateStringsWithoutDigits(String[] strings) {
+        // write your code with StringBuilder here
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < strings.length; i++){
+             String str = strings[i].replaceAll("\\d", "");
+             stringBuilder.append(str);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] strings = scanner.nextLine().split("\\s+");
+        String result = concatenateStringsWithoutDigits(strings);
+        System.out.println(result);
+    }
+}
